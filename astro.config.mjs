a9828072fcb9aa73043de58e8ad93e26a92cc183
José <pablotegascia@gmail.com>
2025-01-29 10:56:38 +0100

    Añadir astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
-    adapter: vercel()
-  });
-=======
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
 });
->>>>>>> 5b08099dd76b73c839cc38778d25239f4167e0a6
